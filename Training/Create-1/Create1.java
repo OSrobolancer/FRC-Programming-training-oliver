9e06d9b2368b66a94983d8477fa0b3b015094ba8
OSrobolancer <9456333@philasd.org>
2026-05-13 15:07:54 -0400

    Update Create1.java

--- Training/Create-1/Create1.java
+++ Training/Create-1/Create1.java
@@ -4,9 +4,9 @@ public class Create1 {
      *  Write a program that creates two integer variables with any numbers. 
      *  Then, add those two numbers together and print the result. 
      */
-    public static int addition(a, b) {
-        return a + b
-            
+    public static int addition(int a, int b) {
+        return a + b;
+    }
     public static void main(String[] args) {
         System.out.println(addition(1, 5));
     }
